75e80a130b49041e28d11ab9bba215eb800dcc8e
Katie <adlerk8@gmail.com>
2020-10-13 14:52:52 -0700

    in progress

--- Part1/index.js
+++ Part1/index.js
@@ -1,8 +1,25 @@
-function play (){
+const board = []
+
+function play (clickedId){
+    const clickedElement = document.getElementById(clickedId)
     const playerSpan = document.getElementById('player');
     if(playerSpan.innerText === 'X'){
         playerSpan.innerText = 'O';
+        clickedElement.innerText = 'X';
+        board[clickedId] = 'X';
     } else {
         playerSpan.innerText = 'X';
+        clickedElement.innerText = 'O';
+        board[clickedId] = 'O';
     }
 }
+
+const topLeft
+const topCenter
+const topRight
+const midLeft
+const midCenter
+const midRight
+const bottomLeft
+const bottomCenter
+const bottomRight
