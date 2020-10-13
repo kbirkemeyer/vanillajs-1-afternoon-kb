c8f6ceb6e2dac7410f515cb7a05c05caa87d85fd
Katie <adlerk8@gmail.com>
2020-10-13 14:42:36 -0700

    initial commit

---
+++ Part1/index.js
@@ -0,0 +1,8 @@
+function play (){
+    const playerSpan = document.getElementById('player');
+    if(playerSpan.innerText === 'X'){
+        playerSpan.innerText = 'O';
+    } else {
+        playerSpan.innerText = 'X';
+    }
+}
