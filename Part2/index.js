cf34a388748a3aaea3c27dade6e3dc76e9e9d215
Katie <adlerk8@gmail.com>
2020-10-13 17:01:42 -0700

    in progress

---
+++ Part2/index.js
@@ -0,0 +1,8 @@
+let idInput = document.getElementById("idInput");
+let colorInput = document.getElementById("colorInput");
+
+// console.log (setId, setColor)
+
+function setCard (){
+    let card = document.getElementById(idInput.value);
+}
